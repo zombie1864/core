1c009fa87dbcf25c4da4fef7180e5034a0396b31
zombie1864 <zombie1864@gmail.com>
2020-12-16 18:28:51 -0500

    updated Js file - basic static template

--- client_app/index.js
+++ client_app/index.js
@@ -1,5 +1,58 @@
 $("document").ready(function() {
-    $('.id').append("<table border='2'><thread><tr><th>data1</th><th>data2</th><th>data3</th></tr></thread><tbody></tbody><tfoot><tr><td><input type='button' value='new row' id='newRow'></td></tr></tfoot></table>")
+    $('.id').append("\
+        <table border='2'>\
+            <thread>\
+                <th>form\
+                    <form>\
+                        <input id='name' type='text'>\
+                        <br>\
+                        <br>\
+                        <input id='age' type='text'>\
+                        <br>\
+                        <br>\
+                        <input id='gender' type='text'>\
+                        <br>\
+                        <br>\
+                        <input id='email' type='text'>\
+                        <br>\
+                        <br>\
+                        <input id='feedback' type='text'>\
+                    </form>\
+                    <th>table\
+                    <table border='1'>\
+                        <tr>\
+                            <th>col_1</th>\
+                            <th>col_2</th>\
+                            <th>col_3</th>\
+                        </tr>\
+                        <tr>\
+                            <td>data</td>\
+                            <td>data</td>\
+                            <td>data</td>\
+                        </tr>\
+                        <tr>\
+                            <td>data</td>\
+                            <td>data</td>\
+                            <td>data</td>\
+                        </tr>\
+                        <tr>\
+                            <td>data</td>\
+                            <td>data</td>\
+                            <td>data</td>\
+                        </tr>\
+                   </table>\
+                    </th>\
+                </th>\
+            </thread>\
+            <tbody></tbody>\
+            <tfoot>\
+                <tr>\
+                    <td>\
+                        <input type='button' value='new row' id='newRow'>\
+                    </td>\
+                </tr>\
+            </tfoot>\
+        </table>")
     $('#newRow').on('click', onClick); 
     function onClick() {
         var addRow = '<tr>'
