10f6b6b61395229b2b43f46bd2c673af9c77e2a9
zombie1864 <zombie1864@gmail.com>
2020-12-16 17:47:31 -0500

    removed tags from the HTML file and into Js file

--- client_app/index.js
+++ client_app/index.js
@@ -1,4 +1,5 @@
 $("document").ready(function() {
+    $('.id').append("<table border='2'><thread><tr><th>data1</th><th>data2</th><th>data3</th></tr></thread><tbody></tbody><tfoot><tr><td><input type='button' value='new row' id='newRow'></td></tr></tfoot></table>")
     $('#newRow').on('click', onClick); 
     function onClick() {
         var addRow = '<tr>'
