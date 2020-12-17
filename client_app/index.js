8be1e8404ec9fbdf970c349f627a1a2c48d33c16
zombie1864 <zombie1864@gmail.com>
2020-12-17 10:01:35 -0500

    added variable table using modular design and generic functions

--- client_app/index.js
+++ client_app/index.js
@@ -1,67 +1,55 @@
 $("document").ready(function() {
+
     $('.id').append("\
         <table border='2'>\
             <thread>\
                 <th>form\
-                    <form>\
+                    <form id='form'>\
+                        <label>name</label>\
                         <input id='name' type='text'>\
                         <br>\
                         <br>\
+                        <label>age</label>\
                         <input id='age' type='text'>\
                         <br>\
                         <br>\
+                        <label>gender</label>\
                         <input id='gender' type='text'>\
                         <br>\
                         <br>\
+                        <label>email</label>\
                         <input id='email' type='text'>\
                         <br>\
                         <br>\
+                        <label>feedback</label>\
                         <input id='feedback' type='text'>\
                     </form>\
-                    <th>table\
-                    <table border='1'>\
-                        <tr>\
-                            <th>col_1</th>\
-                            <th>col_2</th>\
-                            <th>col_3</th>\
-                        </tr>\
-                        <tr>\
-                            <td>data</td>\
-                            <td>data</td>\
-                            <td>data</td>\
-                        </tr>\
-                        <tr>\
-                            <td>data</td>\
-                            <td>data</td>\
-                            <td>data</td>\
-                        </tr>\
-                        <tr>\
-                            <td>data</td>\
-                            <td>data</td>\
-                            <td>data</td>\
-                        </tr>\
-                   </table>\
+                    <th id='varTable'>table\
+                        <table border='1'></table>\
                     </th>\
                 </th>\
             </thread>\
             <tbody></tbody>\
-            <tfoot>\
-                <tr>\
-                    <td>\
-                        <input type='button' value='new row' id='newRow'>\
-                    </td>\
-                </tr>\
-            </tfoot>\
-        </table>")
-    $('#newRow').on('click', onClick); 
-    function onClick() {
-        var addRow = '<tr>'
-        addRow += "<td> <input type='text' name='test1'></td>"
-        addRow += "<td> <input type='text' name='test2'></td>"
-        addRow += "<td> <input type='text' name='test3'></td>"
-        addRow += '</tr>';
+        </table>"
+    )
+    let data = [
+        ['col_1', 'col_2', 'col_3'], 
+        ['data', 'data', 'data'], 
+        ['data', 'data', 'data'], 
+        ['data', 'data', 'data']
+    ]
 
-        $('table tbody').append(addRow)
+    let const_construct_table = data => {
+        let table = $("<table/>");
+        $.each(data, (rowIndex, r) => {
+            let row = $("<tr/>");
+            $.each(r, (colIndex, c) => { 
+                row.append($("<t"+(rowIndex == 0 ?  "h" : "d")+"/>").text(c));
+            });
+            table.append(row);
+        });
+        $('#varTable').append(table)
     }
+    dataTable = const_construct_table(data); 
 }); 
-
+    
