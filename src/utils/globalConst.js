bece2df3314c252a50b8270707921f60435bbdf2
zombie1864 <zombie1864@gmail.com>
2021-09-21 13:30:44 -0400

    rm unused class

--- src/utils/globalConst.js
+++ src/utils/globalConst.js
@@ -1,5 +1,5 @@
 export const webUrl = 'http://127.0.0.1:5000/attr'
-export const tableTag = $('<table id="tableId" class="Yo"/>');
+export const tableTag = $('<table id="tableId"/>');
 export const formLabels = ['Name', 'Age', 'Email', 'Feedback', 'Gender']
 export let dataIDFromDB = null // the ID stored in db to each obj entry in db 
 
